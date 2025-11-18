25c0c7a2507466845353178b8d744477959957e1
Calebabisai <gelndcaleb@gmail.com>
2025-11-18 00:20:59 -0600

    Creacion de Pipe, para poder usarlo en el HTML y muestre las sinopsis hasta un numero de texto limitado, mas adelante implementare un boton que muestre la sinopsis completa

--- src/app/components/movie-card/movie-card.ts
+++ src/app/components/movie-card/movie-card.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Movie } from '../../models/movie-model/movie-model-module';
+import { TruncatePipe } from '../../pipes/truncate-pipe';
 
 @Component({
   selector: 'app-movie-card',
   standalone:true,
-  imports: [],
+  imports: [TruncatePipe],
   templateUrl: './movie-card.html',
   styleUrl: './movie-card.css',
 })
